5f3cff240642adb9c05e7731f8c3c92c749c9039
Jordan Carroll <jordancarroll1994@gmail.com>
2020-10-07 02:44:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,5 @@ function logWhisper (string) {
 }
 function sayHiToGrandma (string) {
   if (string.toLowerCase() === string) {return "I can't hear you!"}
-  else if () {}
-  else {}
+  else if (string.toUpperCase() === string) {return "YES INDEED!"}
 }
