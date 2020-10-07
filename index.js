0f883f494579fc3320b173a64e0eddeb0a80a154
Jordan Carroll <jordancarroll1994@gmail.com>
2020-10-07 01:27:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,5 @@ function whisper (string) {
   return string.toLowerCase()
 }
 function logShout (string) {
-  console.log; string.toUpperCase()
-}
-function logShout (string) { 
-  console.log; string.toUpperCase() 
+  console.log (string.toUpperCase()
 }
